0c950e4321f2672d7c346dbff72f87e0483171f2
Pawel Chowdhury <c.pawel.10@gmail.com>
2024-01-29 14:27:28 -0800

    First commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+void drawBoard(char* spaces);
+void playerMove(char* spaces, char player);
+void computerMove(char* spaces, char computer);
+bool checkWinner(char* spaces, char player, char computer);
+bool checkTie(char* spaces);
+
+
+int main() {
+	//pointers = variables that store memory address
+	//& address of operator
+	//* dereference operator
+	//array names are already an address
+	//dereferencing a pointer to an array displays array
+	//nullptr = keywrod represents a null pointer literal
+	int* pointer = nullptr;
+	if(pointer == nullptr){
+	}
+
+
+
+}
